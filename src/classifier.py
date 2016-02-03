f5ea75b0e72681f9d2bcaf6ef379832523229934
Josh <engelsjo@mail.gvsu.edu>
2016-02-03 14:08:57 -0500

    initial template commit - with data downloaded to repo as well

---
+++ src/classifier.py
@@ -0,0 +1,25 @@
+"""
+@summary: Python script that classifies documents
+@version: 1.0
+@authors: Josh Engelsma, Adam Terwilliger, Michael Baldwin
+@date: Febrary 3, 2016
+"""
+
+import sys
+
+
+class Classifier(object):
+
+    def __init__(self):
+        self.table = {}
+
+
+
+def main(argv):
+    programName = argv[0]
+    c = Classifier()
+
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
