85b7291228f51982c5b200e00a01b7751813aeea
Adam Terwilliger <adamtwig@hotmail.com>
2016-02-06 15:10:14 -0500

    dict class:{docs}, doc nums

--- src/classifier.py
+++ src/classifier.py
@@ -6,20 +6,45 @@
 """
 
 import sys
+import pprint as pp
 
+train_dir = '../data/trainData/'
+test_dir = '../data/testData/'
 
 class Classifier(object):
 
     def __init__(self):
         self.table = {}
-
-
+        self.vocab = {}
+        self.classCount = {}
 
 def main(argv):
     programName = argv[0]
     c = Classifier()
+    dataFile = argv[1]
+    if 'Train' in dataFile:
+        dataFile = train_dir + dataFile
+    else:
+        dataFile = test_dir + dataFile
+   	
+    with open(dataFile, "r") as fh:
+        for line in fh:
+            lineParts = line.split(' ')
+            docClass = lineParts[0]
+            doc = lineParts[1:]
+            doc[-1] = doc[-1].strip('\n')
+            for word in doc:
+                c.vocab[word] = 0
+            if (docClass not in c.table.keys()):
+                c.table[docClass] = doc
+                c.classCount[docClass] = 1
+            else:
+                c.table[docClass] += doc
+                c.classCount[docClass] += 1
+            #print "docClass: ", docClass
+            #print "doc: ", doc
+    pp.pprint(c.classCount)
 
-    
 
 if __name__ == "__main__":
     main(sys.argv)
