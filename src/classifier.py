70507dc6486d01ecd85c0d986e2a277d00d07520
Adam Terwilliger <adamtwig@hotmail.com>
2016-02-06 17:12:08 -0500

    added prob_cj and n_j

--- src/classifier.py
+++ src/classifier.py
@@ -7,6 +7,7 @@
 
 import sys
 import pprint as pp
+import numpy as np
 
 train_dir = '../data/trainData/'
 test_dir = '../data/testData/'
@@ -18,6 +19,15 @@ class Classifier(object):
         self.vocab = {}
         self.classCount = {}
         self.vocab_table = {}
+        self.prob_cj = {}
+        self.nj = {}
+        self.corpusSize = 0
+        self.vocabSizeK = 0
+        self.numClassesJ = 0
+        self.prob_wkcj = np.array()
+
+    def classify():
+        print "hello from the other side"        
 
 def main(argv):
     programName = argv[0]
@@ -31,31 +41,62 @@ def main(argv):
     with open(dataFile, "r") as fh:
         for line in fh:
             lineParts = line.split(' ')
+
+            # get the classes of docs
             docClass = lineParts[0]
+
+            # get each doc corresponding to each class
             doc = lineParts[1:]
+
+            # remove end line character from end word in doc
             doc[-1] = doc[-1].strip('\n')
+            
             if (docClass not in c.table.keys()):
+                # haven't seen class, so add to dict
                 c.table[docClass] = doc
+                # ???????
                 c.vocab_table[docClass] = {}
+                # first time seeing class
                 c.classCount[docClass] = 1
+
             else:
+                # append doc to master doc by class
                 c.table[docClass] += doc
+                # increment number of docs per class
                 c.classCount[docClass] += 1
+
             for word in doc:
+                # use dict for quick check if word
+                #   has already appeared in dict
                 c.vocab[word] = 0
+               
+                # if we have seen word, increment count
                 try:
                     c.vocab_table[docClass][word] +=1 
                 except Exception:
                     # we have not added a word yet
                     c.vocab_table[docClass][word] = 1
-            #print "docClass: ", docClass
-            #print "doc: ", doc
-    #print c.table["baseball"]
-    pp.pprint(c.classCount)
-    print "len vocab:", len(c.vocab)
-    #pp.pprint(c.vocab_table["baseball"])
+    
+    # iterate over 20 classes to get corpusSize
+    for cKey in c.classCount.keys():
+        c.corpusSize += c.classCount[cKey]
+    
+    # probability estimate of a particular class
+    for cKey in c.classCount.keys():
+        count = float(c.classCount[cKey])
+        size = float(c.corpusSize)
+        c.prob_cj[cKey] = count / size 
 
-    print len(c.table["baseball"])
+    # total number of word positions in Text_j
+    for cKey in c.table.keys():
+        c.nj[cKey] = len(c.table[cKey])
+   
+    # get size of matrix
+    vocabSizeK = len(c.vocab)
+    amountClassesJ = len(c.classCount.keys())
+
+    prob_wkcj = np.array([vocabSizeK,amountClassesJ])
+        
 
 if __name__ == "__main__":
     main(sys.argv)
