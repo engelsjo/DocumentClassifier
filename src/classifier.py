accd957a58611fc0f41522c037af6cab773df53c
Michael Baldwin <baldwmic@mail.gvsu.edu>
2016-02-11 21:48:54 -0500

    document term matrix

--- src/classifier.py
+++ src/classifier.py
@@ -6,41 +6,61 @@
 """
 
 import argparse
+import numpy as np
+# np.set_printoptions(threshold=np.nan)
 
 class Classifier(object):
 
-    def __init__(self, arguments):
-        self.arguments = arguments
-        self.vocabulary = {}
-        #self.docTermMatrix = [][]
+    def __init__(self, args):
+        self.args = args
+        # set of unique words
+        self.vocab = set()
+        # two-dimensional array (matrix) of documents and terms (words)
+        self.docTermMatrix = None
 
-    def parseTrain(self, fileName, delimiter):
+    def parseTrain(self, fileName, stripChars, delimiter):
         lines = []
         with open(fileName, 'rb') as fileHandler:
             for line in fileHandler:
-                lineTokens = line.strip('\n\r').split(delimiter)
+                lineTokens = line.strip(stripChars).split(delimiter)
                 lines.append(lineTokens)
         return lines
 
-    def learn(self, fileName, delimiter):
-        linesParsed = self.parseTrain(self.arguments['trainingSet'], ' ')
-        for line in linesParsed:
-            documentClass = line[0]
+    def createVocab(self, lines):
+        for line in lines:
             document = line[1:]
-
-            # collect set of all distinct words
+            # try to add each word to unique set of words (ignores duplicates)
             for word in document:
-                # if word not seen before add as key to vocabulary
-                if word not in self.vocabulary:
-                    self.vocabulary[word] = ''
+                self.vocab.add(word)
 
+    def createWordsToIndices(self, vocab):
+        wordsToIndices = {}
+        index = 0
+        for word in vocab:
+            if word not in wordsToIndices:
+                wordsToIndices[word] = index
+                index += 1
+        return wordsToIndices
 
+    def createMatrix(self, lines, numRows, numCols):
+        self.docTermMatrix = np.zeros((numRows, numCols))
+        wordsToIndices = self.createWordsToIndices(self.vocab)
+        for i in range(0, len(lines)):
+            line = lines[i]
+            document = line[1:]
+            for word in document:
+                self.docTermMatrix[i][wordsToIndices[word]] += 1
 
+    def learn(self, fileName):
+        lines = self.parseTrain(fileName, '\n\r', ' ')
+        self.createVocab(lines)
+        self.createMatrix(lines, len(lines), len(self.vocab))
+        print self.docTermMatrix[0]
 
 def parseCommands():
     # create argument parser
     ArgParser = argparse.ArgumentParser()
-    # add arguments expected to run program
+    # add positional arguments expected to run program
     ArgParser.add_argument('trainingSet',
         help='The set of data to discover potentially predictive relationships.')
     ArgParser.add_argument("testSet",
@@ -51,8 +71,7 @@ def parseCommands():
 
 def main():
     c = Classifier(parseCommands())
-    c.learn()
-
+    c.learn(c.args['trainingSet'])
 
 
 if __name__ == "__main__":
